037116abcfc2c39fcabf006cabd2ef2b2d2ef1c7
WAtuchei <112400074+WAtuchei@users.noreply.github.com>
2024-01-29 23:55:21 +0700

    Fix: re-correction Header

--- src/assets/Header.jsx
+++ src/assets/Header.jsx
@@ -2,16 +2,15 @@ import "../scss/header.scss"
 
 function Header() {
    return (
-      <header className="w-full flex flex-1 items-center">
-         <div className="header-BG w-full h-full"></div>
-            <div className="w-full text-center">
-               <h1>
-                  Simple, traffic-based pricing
-               </h1>
-               <p>
-                  Sign-up for our 30-day trial. No credit card required.
-               </p>
-            </div>
+      <header className="w-full flex flex-1">
+         <div className="header-BG w-full text-center mt-[3rem] px-8 py-16">
+            <h1>
+               Simple, traffic-based pricing
+            </h1>
+            <p className="mt-3 px-16">
+               Sign-up for our 30-day trial. No credit card required.
+            </p>
+         </div>
       </header>
    )
 }
--- src/assets/MainBody.jsx
+++ src/assets/MainBody.jsx
@@ -1,8 +1,15 @@
+import "../scss/mainBody.scss"
+import Slider from "./components/Slider"
 
 function MainBody() {
    return (
-      <div className="container flex-1 bg-white">
-
+      <div className="container flex flex-1 justify-center">
+         <section className="App-body p-8 text-center bg-white">
+            <p className="uppercase">
+               100k pageviews
+            </p>
+            <Slider />
+         </section>
       </div>
    )
 }
