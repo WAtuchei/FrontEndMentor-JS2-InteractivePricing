91c5479b89287dad2af8f559b6e660a548acb27b
WAtuchei <112400074+WAtuchei@users.noreply.github.com>
2024-02-01 23:15:38 +0700

    Style: lower MainBody layout

--- src/assets/MainBody.jsx
+++ src/assets/MainBody.jsx
@@ -4,12 +4,41 @@ import Slider from "./components/Slider"
 function MainBody() {
    return (
       <div className="container flex flex-1 justify-center">
-         <section className="App-body p-8 text-center bg-white">
-            <p className="uppercase">
-               100k pageviews
-            </p>
-            <Slider />
-         </section>
+         <section className="App-body text-center bg-white">
+            <div className="p-8">
+               <p className="uppercase">
+                  100k pageviews
+               </p>
+               <Slider />
+            </div>
+            
+            <section className="container mt-10">
+               <ul className="flex flex-col justify-center items-center">
+                  <li className="flex justify-center mt-2 gap-x-4 items-center text-center">
+                     <img 
+                        src="../../images/icon-check.svg" 
+                        alt="check" 
+                     />
+                     <p>Unlimited websites</p>
+                  </li>
+                  <li className="flex justify-center mt-2 gap-x-4 items-center text-center">
+                     <img 
+                        src="../../images/icon-check.svg" 
+                        alt="check" 
+                     />
+                     <p>100% data ownership</p>
+                  </li>
+                  <li className="flex justify-center mt-2 gap-x-4 items-center text-center">
+                     <img 
+                        src="../../images/icon-check.svg" 
+                        alt="check" 
+                     />
+                     <p>Email reports</p>
+                  </li>
+               </ul>
+               <button>Start my trial</button>
+            </section>
+         </section>     
       </div>
    )
 }
