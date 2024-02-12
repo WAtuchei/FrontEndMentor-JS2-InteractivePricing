3df6747c1b09bff6b3d5278f9a7dc475eccba299
WAtuchei <112400074+WAtuchei@users.noreply.github.com>
2024-02-12 18:04:49 +0700

    Fix some style details

--- src/assets/layouts/UpperBody.jsx
+++ src/assets/layouts/UpperBody.jsx
@@ -3,11 +3,11 @@ import Slider from "../components/Slider"
 
 function UpperBody() {
    return (
-      <div className="container flex flex-col justify-between items-stretch">
+      <div className="container p-8 flex flex-col justify-between items-stretch">
          <div className="flex flex-col my-5">
             <Slider />
          </div>
-         <div className="flex">
+         <div className="flex justify-center">
             <CheckBox />
          </div>
       </div>
